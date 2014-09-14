a7ae1a9bdcaf571ee96caa09e9d85d50b42ce6e7
Federico Ravasio <ravasio.federico@gmail.com>
2014-09-14 14:43:22 +0200

    Use rspec's matcher `be` to compare symbols.

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -17,7 +17,7 @@ describe Hand do
       subject { hand '2h 3h 1h 5h 4h' }
 
       it 'returns :high_card' do
-        expect(subject.rank).to eq :high_card
+        expect(subject.rank).to be :high_card
       end
     end
 
@@ -25,7 +25,7 @@ describe Hand do
       subject { hand '2h 3h 1h 3d 4h' }
 
       it 'returns :one_pair' do
-        expect(subject.rank).to eq :one_pair
+        expect(subject.rank).to be :one_pair
       end
     end
 
@@ -33,7 +33,7 @@ describe Hand do
       subject { hand '2h 3h 2c 3d 4h' }
 
       it 'returns :two_pairs' do
-        expect(subject.rank).to eq :two_pairs
+        expect(subject.rank).to be :two_pairs
       end
     end
 
@@ -41,7 +41,7 @@ describe Hand do
       subject { hand '3d 2h 2s 4h 2c' }
 
       it 'returns :three_of_a_kind' do
-        expect(subject.rank).to eq :three_of_a_kind
+        expect(subject.rank).to be :three_of_a_kind
       end
     end
 
@@ -50,7 +50,7 @@ describe Hand do
         subject { hand '10s 9d 8h 7s 6c' }
 
         it 'returns :straight' do
-          expect(subject.rank).to eq :straight
+          expect(subject.rank).to be :straight
         end
       end
 
@@ -58,7 +58,7 @@ describe Hand do
         subject { hand '1c 2s 3h 4d 5s' }
 
         it 'returns :straight' do
-          expect(subject.rank).to eq :straight
+          expect(subject.rank).to be :straight
         end
       end
 
@@ -66,7 +66,7 @@ describe Hand do
         subject { hand '1s ks qd jc 10c' }
 
         it 'returns :straight' do
-          expect(subject.rank).to eq :straight
+          expect(subject.rank).to be :straight
         end
       end
 
@@ -74,7 +74,7 @@ describe Hand do
         subject { hand '3c 2d 1h kc qs' }
 
         it 'does not return :straight' do
-          expect(subject.rank).not_to eq :straight
+          expect(subject.rank).not_to be :straight
         end
       end
 
@@ -82,7 +82,7 @@ describe Hand do
         subject { hand '10s 9s 8s 7s 6s' }
 
         it 'does not return :straight' do
-          expect(subject.rank).not_to eq :straight
+          expect(subject.rank).not_to be :straight
         end
       end
     end
