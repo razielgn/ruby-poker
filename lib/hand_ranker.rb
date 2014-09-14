4a0a33f2f16971767c8bc66a8fce6ca9c1783720
Federico Ravasio <ravasio.federico@gmail.com>
2014-09-14 17:30:50 +0200

    Extracted rank calculations into HandRanker.

---
+++ lib/hand_ranker.rb
@@ -0,0 +1,103 @@
+class HandRanker
+  def initialize(hand)
+    @cards = hand.cards
+  end
+
+  def rank
+    if straight_flush?
+      :straight_flush
+    elsif four_of_a_kind?
+      :four_of_a_kind
+    elsif full_house?
+      :full_house
+    elsif straight?
+      :straight
+    elsif flush?
+      :flush
+    elsif three_of_a_kind?
+      :three_of_a_kind
+    elsif two_pairs?
+      :two_pairs
+    elsif one_pair?
+      :one_pair
+    else
+      :high_card
+    end
+  end
+
+  private
+
+  attr_reader :cards
+
+  def one_pair?
+    pairs_count == 1
+  end
+
+  def two_pairs?
+    pairs_count == 2
+  end
+
+  def three_of_a_kind?
+    triples_count == 1
+  end
+
+  def straight?
+    in_sequence? && !one_suit?
+  end
+
+  def flush?
+    one_suit? && !in_sequence?
+  end
+
+  def full_house?
+    three_of_a_kind? && one_pair?
+  end
+
+  def four_of_a_kind?
+    fourths_count == 1
+  end
+
+  def straight_flush?
+    one_suit? && in_sequence?
+  end
+
+  def grouped_by_pips
+    cards.group_by(&:pips)
+  end
+
+  def grouped_by_suits
+    cards.group_by(&:suit)
+  end
+
+  def sorted_by_pips
+    cards.sort_by(&:pips)
+  end
+
+  def pairs_count
+    same_pips_count(2)
+  end
+
+  def triples_count
+    same_pips_count(3)
+  end
+
+  def fourths_count
+    same_pips_count(4)
+  end
+
+  def one_suit?
+    grouped_by_suits.count == 1
+  end
+
+  def in_sequence?
+    pips = sorted_by_pips.map(&:pips)
+    pips == [1, 10, 11, 12, 13] ||
+      pips == (pips.first..pips.last).to_a
+  end
+
+  def same_pips_count(count)
+    grouped_by_pips.values.count { |cards|
+      cards.count == count
+    }
+  end
+end
---
+++ spec/hand_ranker_spec.rb
@@ -0,0 +1,85 @@
+require 'hand_ranker'
+
+RSpec::Matchers.define :have_rank do |expected_rank|
+  match { |string|
+    HandRanker.new(hand string).rank == expected_rank
+  }
+end
+
+describe HandRanker do
+  describe '#rank' do
+    example 'high card' do
+      expect('2h 3d kd 5h 4h').to have_rank :high_card
+    end
+
+    example 'one pair' do
+      expect('2h 3h 1h 3d 4h').to have_rank :one_pair
+    end
+
+    example 'two pairs' do
+      expect('2h 3h 2c 3d 4h').to have_rank :two_pairs
+    end
+
+    example 'three of a kind' do
+      expect('3d 2h 2s 4h 2c').to have_rank :three_of_a_kind
+    end
+
+    context 'straight' do
+      example 'ten-high' do
+        expect('10s 9d 8h 7s 6c').to have_rank :straight
+      end
+
+      example 'wheel' do
+        expect('1c 2s 3h 4d 5s').to have_rank :straight
+      end
+
+      example 'ace high' do
+        expect('1s ks qd jc 10c').to have_rank :straight
+      end
+
+      example 'wrap around' do
+        expect('3c 2d 1h kc qs').not_to have_rank :straight
+      end
+
+      example 'less than two suits' do
+        expect('10s 9s 8s 7s 6s').not_to have_rank :straight
+      end
+    end
+
+    context 'flush' do
+      example 'same suit, not in sequence' do
+        expect('10s 9s 8s 7s 5s').to have_rank :flush
+      end
+
+      example 'same suit, in sequence' do
+        expect('10s 9s 8s 7s 6s').not_to have_rank :flush
+      end
+    end
+
+    example 'full house' do
+      expect('10s 10h 10d 4s 4d').to have_rank :full_house
+    end
+
+    example 'four of a kind' do
+      expect('10s 10h 10d 10c 4d').to have_rank :four_of_a_kind
+    end
+
+    context 'straight flush' do
+      example 'ten-high' do
+        expect('10s 9s 8s 7s 6s').to have_rank :straight_flush
+      end
+
+      example 'wheel' do
+        expect('1c 2c 3c 4c 5c').to have_rank :straight_flush
+      end
+
+      example 'ace-high' do
+        expect('1s ks qs js 10s').to have_rank :straight_flush
+      end
+
+      example 'wrap around' do
+        expect('3c 2c 1c kc qc').not_to have_rank :straight_flush
+      end
+    end
+  end
+end
