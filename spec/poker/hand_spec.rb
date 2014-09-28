df00f98d52630d6aedae22aaecebde0e2d714ef6
Federico Ravasio <ravasio.federico@gmail.com>
2014-09-28 15:57:52 +0200

    Refactored specs around Poker::Hand.new

--- spec/poker/hand_spec.rb
+++ spec/poker/hand_spec.rb
@@ -3,30 +3,57 @@ require 'poker/hand'
 module Poker
   describe Hand do
     describe '.new' do
-      it 'raises InvalidHandError when given amount different than 5' do
-        expect {
-          described_class.new([])
-        }.to raise_error(InvalidHandError)
+      let(:cards) {
+        [
+          card('1h'),
+          card('2h'),
+          card('3h'),
+          card('4h'),
+          card('5h'),
+          card('6h'),
+        ]
+      }
 
-        expect {
-          described_class.new([:a])
-        }.to raise_error(InvalidHandError)
+      it 'raises InvalidHandError for no cards' do
+        expect do
+          described_class.new(cards.take(0))
+        end.to raise_error(InvalidHandError)
+      end
 
-        expect {
-          described_class.new([:a, :b])
-        }.to raise_error(InvalidHandError)
+      it 'raises InvalidHandError for one card' do
+        expect do
+          described_class.new(cards.take(1))
+        end.to raise_error(InvalidHandError)
+      end
 
-        expect {
-          described_class.new([:a, :b, :c])
-        }.to raise_error(InvalidHandError)
+      it 'raises InvalidHandError for two cards' do
+        expect do
+          described_class.new(cards.take(2))
+        end.to raise_error(InvalidHandError)
+      end
 
-        expect {
-          described_class.new([:a, :b, :c, :d])
-        }.to raise_error(InvalidHandError)
+      it 'raises InvalidHandError for three cards' do
+        expect do
+          described_class.new(cards.take(3))
+        end.to raise_error(InvalidHandError)
+      end
 
-        expect {
-          described_class.new([:a, :b, :c, :d, :e, :f])
-        }.to raise_error(InvalidHandError)
+      it 'raises InvalidHandError for four cards' do
+        expect do
+          described_class.new(cards.take(4))
+        end.to raise_error(InvalidHandError)
+      end
+
+      it 'raises InvalidHandError for six cards' do
+        expect do
+          described_class.new(cards.take(6))
+        end.to raise_error(InvalidHandError)
+      end
+
+      it 'does not raise InvalidHandError for five cards' do
+        expect do
+          described_class.new(cards.take(5))
+        end.not_to raise_error
       end
     end
   end
